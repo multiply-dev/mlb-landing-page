667c1d9b1ae1e49e72d162addd114706f75f75e0
multiply-dev <jacob.rubin@multiply.us>
2025-05-26 14:20:26 -0500

    Update build

--- src/components/CouponModal/CouponModal.js
+++ src/components/CouponModal/CouponModal.js
@@ -1,20 +1,42 @@
 import './CouponModal.css';
+import { useEffect, useState } from "react";
 
 const CouponModal = ({ prize, prizeColor }) => {
+  const [coupon, setCoupon] = useState("");
+
+  useEffect(() => {
+    const getCoupon = async () => {
+      if (prize) {
+        try {
+          const response = await fetch(`https://script.google.com/macros/s/AKfycbzVtZp2T6YtZc_IsNnG3wYU-f2QuH2CFY7eV3f9VjDFn3G-w5W-32O03EL24_9dbSWn/exec?prize=${encodeURIComponent(prize)}`);
+          const data = await response.json();
+          setCoupon(data.coupon || "No coupon available");
+        } catch (error) {
+          console.error("Error fetching coupon:", error);
+          setCoupon("Error retrieving coupon");
+        }
+      }
+    };
+
+    getCoupon();
+  }, [prize]);
 
   return (
     <div className="coupon-modal-overlay">
       <div className={`coupon-modal-content ${prizeColor}`}>
-          <div>
-            <h1 className="coupon-text">Coupon Code:</h1>
-            <h3 className="coupon-subtext">[insert-coupon-name]</h3>
-          </div>
-          <div>
-            <h3>Visit <a href="https://www.hi-chew.com/" className="hi-chew-link" target="_blank" rel="noreferrer">HI-CHEW<sup>®</sup></a> and use this code at checkout</h3>
-          </div>
+        <div>
+          <h1 className="coupon-text">Coupon Code:</h1>
+          <h3 className="coupon-subtext">{coupon}</h3>
         </div>
+        <div>
+          <h3>
+            Visit <a href="https://www.hi-chew.com/" className="hi-chew-link" target="_blank" rel="noreferrer">HI-CHEW<sup>®</sup></a> and use this code at checkout
+          </h3>
+        </div>
+      </div>
     </div>
   );
 };
 
 export default CouponModal;
+
